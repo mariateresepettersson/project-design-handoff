758ea0e522223a068a930a8e6919f26784c2471b
mariateresepettersson <mariateresepettersson>
2023-11-02 11:55:10 +0000

    Creating routes for home and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
-export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+// The main App component sets up routing for the application.
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './pages/HomePage';
+import { About } from './pages/AboutPage';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
